547723b40630cd6a917a1096c03d4e67ff2196c6
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-01-26 14:55:47 +0000

    fleshed out socket tests

--- test/unit/services/socketIOClient_spec.js
+++ test/unit/services/socketIOClient_spec.js
@@ -17,7 +17,4 @@ describe('Unit: socketIOClientService', function () {
   it('should exist', function () {
     expect(service).toBeDefined();
   });
-  xit('should set the transport to only use polling');
-  xit('should set the url');
-  xit('should set the sdk info keys up');
 });
--- test/unit/services/socket_spec.js
+++ test/unit/services/socket_spec.js
@@ -12,7 +12,7 @@ describe('Unit: SocketService', function () {
     module(function ($provide) {
       $provide.value('sailsIOClientService', function () {
         return {
-          sails: {sdk: {}}
+          sails: {sdk: {version: "12", lib: "bar"}}
         };
       });
       $provide.value('socketIOClientService', function () {
@@ -29,4 +29,18 @@ describe('Unit: SocketService', function () {
     expect(service).toBeDefined();
   });
 
+  it('should set the transport to only use polling', function () {
+    expect(service.sails.transports).toEqual(['polling']);
+  });
+
+  it('should set the url', function () {
+    expect(service.sails.url).toBeDefined();
+  });
+
+  it('should set the sdk info keys up', function () {
+    expect(service.sails.query).toEqual({
+      __sails_io_sdk_version: '12',
+      __sails_io_sdk_lib: 'bar'
+    });
+  });
 });
