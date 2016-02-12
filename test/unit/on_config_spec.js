28e77a5d25a2090880196f49b304f0049893f1a9
OllieNilsen <ollie@ollienilsen.co.uk>
2016-02-12 14:18:08 +0000

    BM499 stubbed unit tests

---
+++ test/unit/on_config_spec.js
@@ -0,0 +1,19 @@
+describe('UNIT on_config.js', function(){
+
+  xit('should exist.', function(){
+
+  });
+
+  xit('should enable html5mode on locationProvider.', function(){
+
+  });
+
+  xit('should call state() on stateProvider with the right parameters.', function(){
+
+  });
+
+  xit('should call otnerwise() on urlRouterProvider with the right params.', function(){
+
+  });
+
+});
