b345ae4396dc59994304545c98cbe365aa1aceb8
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-04-06 12:47:48 +0100

    remove image minification, uglification and fonts

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -11,7 +11,6 @@ gulp.task('watch', ['browserSync'], function() {
   gulp.watch(config.scripts.src, ['lint']);
   gulp.watch(config.styles.src,  ['styles']);
   gulp.watch(config.images.src,  ['images']);
-  gulp.watch(config.fonts.src,   ['fonts']);
   gulp.watch(config.views.watch, ['views']);
 
 });
