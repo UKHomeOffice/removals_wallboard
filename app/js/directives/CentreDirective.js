792debc87ba1094ca4de080d6f925e26a97a73bc
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-04-11 13:05:42 +0100

    more explicit comparators

--- app/js/directives/CentreDirective.js
+++ app/js/directives/CentreDirective.js
@@ -10,13 +10,13 @@ function CentreDirective() {
     link: function (scope, element, attrs) {
       scope.$watch('attributes.maleAvailability', function (newvalue) {
         scope.attributes.highlightMale = newvalue < 1 ? 'highlight' : '';
-        scope.attributes.maleAvailability = newvalue == 0 ? 'FULL' : newvalue;
+        scope.attributes.maleAvailability = newvalue === 0 ? 'FULL' : newvalue;
         scope.attributes.statusMale = newvalue < 1 ? 'No Available Male Beds' : 'Available Male Beds';
 
       });
       scope.$watch('attributes.femaleAvailability', function (newvalue) {
         scope.attributes.highlightFemale = newvalue < 1 ? 'highlight' : '';
-        scope.attributes.femaleAvailability = newvalue == 0 ? 'FULL' : newvalue;
+        scope.attributes.femaleAvailability = newvalue === 0 ? 'FULL' : newvalue;
         scope.attributes.statusFemale = newvalue < 1 ? 'No Available Female Beds' : 'Available Female Beds';
 
       });
