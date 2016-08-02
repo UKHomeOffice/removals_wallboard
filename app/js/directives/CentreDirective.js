f77c8e6f15957e9e24c7b318760c82483e80a275
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-08-02 13:15:19 +0100

    objectEquality doesn't need to be et

--- app/js/directives/CentreDirective.js
+++ app/js/directives/CentreDirective.js
@@ -12,13 +12,12 @@ function CentreDirective() {
         scope.highlightMale = newvalue < 1 ? 'highlight' : '';
         scope.maleAvailability = newvalue === 0 ? 'FULL' : newvalue;
         scope.statusMale = newvalue < 1 ? 'No Available Male Beds' : 'Available Male Beds';
-      }, true);
+      });
       scope.$watch('attributes.femaleAvailability', function (newvalue) {
         scope.highlightFemale = newvalue < 1 ? 'highlight' : '';
         scope.femaleAvailability = newvalue === 0 ? 'FULL' : newvalue;
         scope.statusFemale = newvalue < 1 ? 'No Available Female Beds' : 'Available Female Beds';
-
-      }, true);
+      });
     },
   };
 }
